48e749bbb84029404b26a748bedfb15354e2a252
zurte <152751757+zurte@users.noreply.github.com>
2026-06-16 19:20:42 -0300

    Add files via upload

--- Código fonte/JogoView.py
+++ Código fonte/JogoView.py
@@ -4,13 +4,13 @@ import ttkbootstrap as tb
 class App(tb.Window):
 	#função de inicialização
 	def __init__(self):
-		#iniciando a janela usando o tema darkly
-		super().__init__(themename='darkly',title='Jogo Da Discordia')
+		#iniciando a janela
+		super().__init__(title='Jogo Da Discordia')
 		self.geometry('1024x576')
 		
 		#criando o primeiro frame
-		frame_principal = tb.Frame(self,bootstyle='primary')
-		frame_principal.place(relx=0.0,rely=0.0,relwidth=1.0,relheight=1.0)
+		frame_principal = tb.LabelFrame(self,text='Entre amigos e Inimigos')
+		frame_principal.place(relx=0.01,rely=0.01,relwidth=0.98,relheight=0.98)
 		#cria uma lista que vai guardar os nomes das classes que vamos criar ainda, que no caso são as telas propriamente ditas
 		self.frames = {}
 		
@@ -19,38 +19,44 @@ class App(tb.Window):
 			frame = f(parent=frame_principal,controller=self)
 			self.frames[f] = frame
 			
-			frame.place(relx=0.1,rely=0.2)
+			frame.place(relx=0.0,rely=0.0,relwidth=1.0,relheight=1.0)
 			
 		self.mostrar_frame(TelaMenu)
 	
-	#função que puxa um frame pra frente	
+	#função que puxa um frame pra frente    
 	def mostrar_frame(self,nome_do_frame):
-			frame = self.frames[nome_do_frame]
-			frame.tkraise()
+		frame = self.frames[nome_do_frame]
+		frame.tkraise()
 			
 class TelaMenu(tb.Frame):
 	#função de inicialização
 	def __init__(self,parent,controller):
 		super().__init__(parent)
 
-		#criando as labels
-		label_1 = tb.Label(self,text='Label em contrução')
-		label_1.place(relx=0.5,rely=0.5)
+		frame_menu = tb.LabelFrame(self,text='teste de framelabel')
+		frame_menu.place(relx=0.1,rely=0.1, relwidth=0.8, relheight=0.8)
 
-		bt_1 = tb.Button(self,text='Botão em contrução',command = lambda:controller.mostrar_frame(TelaJogo),bootstyle='secondary')
-		bt_1.place(relx=0.5,rely=0.8)
+		#criando as labels
+		label_1 = tb.Label(frame_menu,text='Label em contrução')
+		label_1.pack(pady=5)
+
+		bt_1 = tb.Button(frame_menu,text='Botão em contrução',command = lambda:controller.mostrar_frame(TelaJogo))
+		bt_1.pack(pady=5)
 		
 class TelaJogo(tb.Frame):
 	#função de inicialização
 	def __init__(self,parent,controller):
 		super().__init__(parent)
+
+		frame_jogo = tb.LabelFrame(self,text='teste de framelabel')
+		frame_jogo.place(relx=0.1,rely=0.1, relwidth=0.8, relheight=0.8)
 		
 		#criando as labels
-		label_1 = tb.Label(self,text='Label em contrução 2')
-		label_1.place(relx=0.5,rely=0.5)
+		label_1 = tb.Label(frame_jogo,text='Label em contrução 2')
+		label_1.pack(pady=5)
 		
-		bt_1 = tb.Button(self,text='Botão em contrução 2',command = lambda:controller.mostrar_frame(TelaMenu))
-		bt_1.place(relx=0.5,rely=0.8)
+		bt_1 = tb.Button(frame_jogo,text='Botão em contrução 2',command = lambda:controller.mostrar_frame(TelaMenu))
+		bt_1.pack(pady=5)
 		
 if __name__ == '__main__':
 	app = App()
